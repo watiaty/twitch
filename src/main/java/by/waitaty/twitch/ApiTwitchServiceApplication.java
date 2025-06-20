3c4b63bf3dbfd3b30a214dbbd2013d078b260d9b
Yauheni Haikou <yauheni.haikou@gmail.com>
2025-06-20 16:49:23 +0300

    init twitch service

---
+++ src/main/java/by/waitaty/twitch/ApiTwitchServiceApplication.java
@@ -0,0 +1,13 @@
+package by.waitaty.twitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTwitchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTwitchServiceApplication.class, args);
+	}
+
+}
